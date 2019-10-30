d479cf12a72be2bc753cbc44172ddce626833ed7
PJMantis <54689263+PJMantis@users.noreply.github.com>
2019-10-30 18:51:29 -0400

    Add files via upload
    I've made some changes to my two original function so that they now can work perfectly with Xinru's function.
    Also I started the solve_lazor() function.
    I've achieved the task of generating a combination of blocks, for example, when there are 3 "A" blocks and 1 "C" block. A list will be generated at the very first of this function as [('A', 'A', 'C', 'A'), ('A', 'A', 'A', 'C'), ('A', 'C', 'A', 'A'), ('C', 'A', 'A', 'A')].
    
    But I still have no idea of solving the lazor cuz there would be three situations:
    1. There is no place for next block before you put all of them.
    2. All blocks have been putted into some places but some target points are not passed.
    3. All target points are hit but there are remaining blocks. ( happens a lot when there are a lot of black blocks)
    
    Anyway, I hope you guys can think about it when you have time. If you have different ideas, remember to put them in a new branch.

--- Lazor Project.py
+++ Lazor Project.py
@@ -1,5 +1,5 @@
 '''
-每次Update，在此处写上更新的内容和新实现的功能
+finish read_bff
 '''
 
 
@@ -10,12 +10,61 @@ class Lazor():
     def __init__(self):
         '''
         '''
-        pass
 
-    def read_bff(self):
+    def read_bff(self,filename):
         '''
+        This function is used to read a .bff file and generate a corresponding dictionary.(Written by Xinru)
+        The generated dictionary contains 4 parts.
+
+        *** map ***
+        A list of lists which shows the look of the map.
+
+        *** block ***
+        A list gives us the information about numbers of different types of blocks.
+
+        *** lazer ***
+        A dictionary gives us the information including numbers of lazers, starting positon and the direction of lazers.
+        {(starting position):[direction]}
+
+        *** target_point ***
+        A list gives us the information about the position of targets.
         '''
-        pass
+        # Open the specified file.
+        f = open(filename, "r")
+        file = {}
+        map = []
+        # Skip the line before "GRID START"
+        for line in f:
+            if line.strip("\n") == "GRID START":
+                break
+        for line in f:
+            # Skip the line after "GRID STOP"
+            if line.strip("\n") == "GRID STOP":
+                break
+            # Generate a list of lists with information of the map(the line between "GRID START" and "GRID STOP" )
+            map.append(line.strip("\n").split(" "))
+        # Add map to dictionary.
+        file["map"] = map
+        block = []
+        lazer = {}
+        point = []
+        for line in f:
+            # Generate list/dictionary with information of blocks/lazers/target points.
+            if line.startswith('A') or line.startswith('B') or line.startswith('C'):
+                block.append(line.strip("\n").split(" "))
+            if line.startswith('L'):
+                lst = line.strip("\n").split(" ")
+                lazer[(int(lst[1]),int(lst[2]))] = [int(lst[3]),int(lst[4])]
+            if line.startswith('P'):
+                lst = line.strip("\n").split(" ")
+                point.append((int(lst[1]),int(lst[2])))
+        # Add to the dictionary.
+        file["block"] = block
+        file["lazer"] = lazer
+        file["target_point"] = point
+        # Close the file
+        f.close()
+        return file
 
     def load_lazor_map(self, info_dict):
         '''
@@ -195,4 +244,5 @@ if __name__ == "__main__":
     a = Lazor()
     b = a.load_lazor_map(info_dict)
     c = a.lazor_path(b)
-    print(b)
+    d = a.read_bff('mad_1.bff')
+    print(d)
